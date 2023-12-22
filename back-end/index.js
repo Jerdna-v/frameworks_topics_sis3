9073d290800bd5b4884f6b17ebd60b93473d73cd
klen <klen.copic@gmail.com>
2023-12-22 15:39:01 +0100

    T5_solution backend removing extras

--- back-end/index.js
+++ back-end/index.js
@@ -10,24 +10,6 @@ const port = process.env.PORT || 5000
 
 app.use(cookieParser());
 
-// // set a cookie
-app.use(function (req, res, next) {
-    // check if client sent cookie
-    var cookie = req.cookies.cookieName;
-    if (cookie === undefined) {
-      // no: set a new cookie
-      var randomNumber=Math.random().toString();
-      randomNumber=randomNumber.substring(2,randomNumber.length);
-      res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-      console.log('cookie created successfully');
-    } else {
-      // yes, cookie was already present 
-      console.log('cookie exists', cookie);
-    } 
-    next(); // <-- important!
-  });
-
-
 // Configuration if we had cross origin enabled.
 // let sess = {
 //     secret: 'our litle secret',
