6f64a20d71886643c9c17e75e547a2c4bcc23562
Jerdna-v <natevandrej@gmail.com>
2025-01-25 02:57:33 +0100

    [Update] Deleted Unnecessary Novice CRUD

--- back-end/db/dbConn.js
+++ back-end/db/dbConn.js
@@ -200,51 +200,5 @@ dataPool.oneMachine=(mid)=>{
   })
 }
 
-// dataPool.allNovice=()=>{
-//   return new Promise ((resolve, reject)=>{
-//     conn.query(`SELECT * FROM news`, (err,res)=>{
-//       if(err){return reject(err)}
-//       return resolve(res)
-//     })
-//   })
-// }
-
-// dataPool.oneNovica=(id)=>{
-//   return new Promise ((resolve, reject)=>{
-//     conn.query(`SELECT * FROM news WHERE id = ?`, id, (err,res)=>{
-//       if(err){return reject(err)}
-//       return resolve(res)
-//     })
-//   })
-// }
-
-// dataPool.creteNovica=(title,slug,text,file)=>{
-//   return new Promise ((resolve, reject)=>{
-//     conn.query(`INSERT INTO news (title,slug,text, file) VALUES (?,?,?,?)`, [title, slug, text, file], (err,res)=>{
-//       if(err){return reject(err)}
-//       return resolve(res)
-//     })
-//   })
-// }
-
-// dataPool.AuthUser=(username)=>
-// {
-//   return new Promise ((resolve, reject)=>{
-//     conn.query('SELECT * FROM user_login WHERE user_name = ?', username, (err,res, fields)=>{
-//       if(err){return reject(err)}
-//       return resolve(res)
-//     })
-//   })  
-	
-// }
-
-// dataPool.AddUser=(username,email,password)=>{
-//   return new Promise ((resolve, reject)=>{
-//     conn.query(`INSERT INTO user_login (user_name,user_email,user_password) VALUES (?,?,?)`, [username, email, password], (err,res)=>{
-//       if(err){return reject(err)}
-//       return resolve(res)
-//     })
-//   })
-// }
 module.exports = dataPool;
 
